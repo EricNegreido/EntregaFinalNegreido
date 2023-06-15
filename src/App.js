0f9375e65ef2ecfa5618c88b2e38489c280b0026
Eric Negreido <ericnegreidooo@gmail.com>
2023-06-15 16:20:22 -0300

    components itemlist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {NavBar} from "./components/NavBar"
+import {ItemListContainer} from "./components/ItemListContainer"
 function App() {
-  return <NavBar/>
+  return <>
+    <NavBar/>
+    <ItemListContainer greeting ="Hola bienvendios a mi tienda de ambos"/>
+  </>
 }
 
 export default App;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,4 @@
+export const ItemListContainer = (props) => 
+<div style={{background:'#f2f2f2', padding:'10px', borderRadius:'10px', width:'50%', margin:'5px auto', textAlign:'center'}}>
+    <p style={{color:'#000', fontSize:"20px"}}> {props.greeting} </p>
+</div>
