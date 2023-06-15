0b0efe6bc26985d723b63b243478606f4e263b0e
Eric Negreido <ericnegreidooo@gmail.com>
2023-06-15 14:02:35 -0300

    components navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+export const NavBar = () => 
+<header> 
+    <nav className="navBar">
+        <div className="logo">TODO AMBOS</div>
+        <ul>
+        <a href="*"><li className="navButtons">  Mujeres  </li> </a>
+        <a href="*"><li className="navButtons">  Hombres  </li> </a>
+        <a href="*"><li className="navButtons">  Combos  </li> </a>
+        </ul>
+    </nav>
+</header>
